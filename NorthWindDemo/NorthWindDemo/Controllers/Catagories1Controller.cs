225e3b562aae1db88fc0843034c915ef8a24140f
kylehuang <kaihuang810704@gmail.com>
2018-11-11 21:28:59 +0800

    create controller and view for CRUD

---
+++ NorthWindDemo/NorthWindDemo/Controllers/Catagories1Controller.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using NorthWindDemo.Models;
+
+namespace NorthWindDemo.Controllers
+{
+    public class Catagories1Controller : Controller
+    {
+        private NorthwindEntities1 db = new NorthwindEntities1();
+
+        // GET: Catagories1
+        public ActionResult Index()
+        {
+            return View(db.Catagory.ToList());
+        }
+
+        // GET: Catagories1/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Catagory catagory = db.Catagory.Find(id);
+            if (catagory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(catagory);
+        }
+
+        // GET: Catagories1/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Catagories1/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CategoryID,CategoryName,Description")] Catagory catagory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Catagory.Add(catagory);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(catagory);
+        }
+
+        // GET: Catagories1/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Catagory catagory = db.Catagory.Find(id);
+            if (catagory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(catagory);
+        }
+
+        // POST: Catagories1/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CategoryID,CategoryName,Description")] Catagory catagory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(catagory).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(catagory);
+        }
+
+        // GET: Catagories1/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Catagory catagory = db.Catagory.Find(id);
+            if (catagory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(catagory);
+        }
+
+        // POST: Catagories1/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Catagory catagory = db.Catagory.Find(id);
+            db.Catagory.Remove(catagory);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
