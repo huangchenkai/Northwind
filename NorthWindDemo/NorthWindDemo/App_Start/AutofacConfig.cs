4114547b5c434f07bb8fb10c4db789eefa445df6
kylehuang <kaihuang810704@gmail.com>
2018-11-12 14:19:34 +0800

    1.Add AutofacConfig.cs 2.Global.asax register AutofacConfig 3.Add IAutoInject,cs interface

---
+++ NorthWindDemo/NorthWindDemo/App_Start/AutofacConfig.cs
@@ -0,0 +1,69 @@
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Http;
+
+namespace NorthWindDemo
+{
+    public static class AutofacConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Base set-up
+            var builder = new ContainerBuilder();
+
+            // Register your Web API controllers.
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly()).PropertiesAutowired();
+
+            // OPTIONAL: Register the Autofac filter provider.
+            builder.RegisterWebApiFilterProvider(config);
+
+            //
+
+            var baseType = typeof(IAutoInject);
+
+            builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
+                            .Where(t => baseType.IsAssignableFrom(t) && t != baseType)
+                            .AsImplementedInterfaces().InstancePerLifetimeScope().PropertiesAutowired();
+
+
+            // Register dependencies
+            //SetUpRegistration(builder);
+
+
+            // Build registration.
+            var container = builder.Build();
+
+            // Set the dependency resolver to be Autofac.
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+
+
+        //private static void SetUpRegistration(ContainerBuilder builder)
+        //{
+        //    string As400 = ConfigurationManager.ConnectionStrings["As400"].ToString();
+        //    string Oracle = ConfigurationManager.ConnectionStrings["OracleDbContext"].ToString();
+
+
+        //    builder.Register(x => new iDB2Connection(As400))
+        //           .As<iDB2Connection>()
+        //           .InstancePerLifetimeScope();
+
+        //    builder.Register(x => new OracleConnection(Oracle))
+        //          .As<OracleConnection>()
+        //          .InstancePerLifetimeScope();
+
+        //    builder.RegisterType<Entities>().As<DbContext>().InstancePerRequest();
+
+
+
+        //}
+
+
+
+    }
+}
